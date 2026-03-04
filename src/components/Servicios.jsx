e195f6eae89262bc85906ff27b9738f5489cddf5
loli-digital <loladigitalizada@gmail.com>
2026-03-04 12:00:53 +0100

    Corrección formas detrás de las cards
    Corrección de la forma que se encuentra en las siguientes secciones y/o páginas:
    - Servicios en la page "Home"
    - Mis citas
    - Reservar cita
    
    Con overflow-hidden

--- src/components/Servicios.jsx
+++ src/components/Servicios.jsx
@@ -10,7 +10,7 @@ function Servicios() {
 
       {/* Forma para detrás de las cards */}
 
-      <div className='w-5xl absolute top-40 lg:top-5 z-0 pointer-events-none drop-shadow-[0_0_4px] drop-shadow-cyan-800'>
+      <div className='w-5xl absolute top-40 lg:top-5 z-0 overflow-hidden pointer-events-none drop-shadow-[0_0_4px] drop-shadow-cyan-800'>
         <svg viewBox='0 0 200 200' xmlns='http://www.w3.org/2000/svg'>
           <path fill='#CEFAFE'
             d='M42.7,-71.6C55.9,-66.2,67.8,-56.1,73.8,-43.4C79.9,-30.6,80.2,-15.3,77.8,-1.4C75.4,12.6,70.3,25.1,65,39.1C59.7,53.1,54.1,68.6,43.2,78.6C32.3,88.6,16.2,93.2,0.4,92.5C-15.4,91.8,-30.8,85.9,-42.7,76.5C-54.7,67.1,-63.2,54.2,-71.7,40.9C-80.2,27.6,-88.7,13.8,-91.2,-1.4C-93.6,-16.6,-90,-33.2,-80.3,-44.6C-70.6,-55.9,-54.9,-61.9,-40.5,-66.6C-26.1,-71.2,-13.1,-74.5,0.8,-75.9C14.7,-77.3,29.4,-76.9,42.7,-71.6Z'
