67c1b191785df62111ca0df5dabceeabd5d19d79
Loli <loladigitalizada@gmail.com>
2026-05-19 21:40:22 +0200

    ♻️: refactor route for Home sections
    - HeroSection
    - Servicios
    - Profesionales
    - Reviews
    - Contacto
    - CTA

--- src/pages/Home/sections/CTA.jsx
+++ src/pages/Home/sections/CTA.jsx
@@ -1,27 +1,25 @@
 import { Link } from "react-router-dom";
 
 function CTA() {
-
   return (
-
     <section className="w-full h-96 relative bg-[url(assets/img/armario-medico-totalmente-equipado.jpg)] bg-top bg-fixed bg-cover">
-
       {/* Container con sombra encima de la imagen */}
       <div className="w-full h-96 absolute top-0 bg-cyan-950/90"></div>
 
       <div className="w-full h-96 relative flex flex-col justify-center items-center gap-12">
-        <h2 className="text-3xl text-center text-cyan-50 font-bold px-5 sm:px-0">Tratamientos personalizados para resultados duraderos</h2>
+        <h2 className="text-3xl text-center text-cyan-50 font-bold px-5 sm:px-0">
+          Tratamientos personalizados para resultados duraderos
+        </h2>
 
         {/* Botón para reservar cita */}
         <Link
-          to='/reservar-cita'
-          className='bg-cyan-700 text-white p-3 rounded-sm shadow-[0_0_5px_black] z-50'
+          to="/reservar-cita"
+          className="bg-cyan-700 text-white p-3 rounded-sm shadow-[0_0_5px_black] z-50"
         >
           Reserva cita ya
         </Link>
       </div>
     </section>
-
   );
 }
 
