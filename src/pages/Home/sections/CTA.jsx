07011155b77c9e966a6ef8616a764eeb895efbcd
loli-digital <loladigitalizada@gmail.com>
2026-06-09 19:53:06 +0200

    💄 style: Deleted px-0 in mobile

--- src/pages/Home/sections/CTA.jsx
+++ src/pages/Home/sections/CTA.jsx
@@ -7,7 +7,7 @@ function CTA() {
       <div className="w-full h-96 absolute top-0 bg-cyan-950/90"></div>
 
       <div className="w-full h-96 relative flex flex-col justify-center items-center gap-12">
-        <h2 className="text-3xl text-center text-cyan-50 font-bold px-5 sm:px-0">
+        <h2 className="text-3xl text-center text-cyan-50 font-bold px-5">
           Tratamientos personalizados para resultados duraderos
         </h2>
 
