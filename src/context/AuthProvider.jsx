0cbb654252d0fd72994439daa41decd565fc7153
loli-digital <loladigitalizada@gmail.com>
2026-07-02 15:56:39 +0200

    ✨ feat: Add AuthContext and AuthProvider to connect with Firebase

---
+++ src/context/AuthProvider.jsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+import { auth } from "@/firebase";
+import { AuthContext } from "./AuthContext";
+import { onAuthStateChanged } from "firebase/auth";
+
+function AuthProvider({children}){
+
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser);
+            setLoading(false);
+        });
+
+        return () => unsubscribe();
+    }, []);
+
+    return (
+        <AuthContext.Provider value={{ user, loading }}>
+            {children}
+        </AuthContext.Provider>
+    );
+}
+
+export default AuthProvider;
