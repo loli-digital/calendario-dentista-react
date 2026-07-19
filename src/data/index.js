3ad1e90f3bfd5f5e142b03203f219dfbb407d1ea
Loli <loladigitalizada@gmail.com>
2026-07-19 14:38:13 +0200

    ♻️ refactor: Add export "facturas" from "facturasData"

--- src/data/index.js
+++ src/data/index.js
@@ -2,3 +2,4 @@ export { profesionales } from "./profesionalesData";
 export { reviews } from "./reviewsData";
 export { serviciosCard } from "./serviciosCardData";
 export { serviciosCita } from "./serviciosData";
+export { facturas } from "./facturasData";
