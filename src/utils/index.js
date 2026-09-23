7e3c92970239205f0703a483e0cb4b5222f9a7de
loli-digital <loladigitalizada@gmail.com>
2026-09-23 18:07:23 +0200

    ✨ feat: Add isTodayUnavailable function

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,3 +2,4 @@ export { validatePhone } from "./validatePhone";
 export { validateDocumentNumber } from "./validateDocumentNumber";
 export { filterPastHours } from "./filterPastHours";
 export { getUserDisplayName } from "./getUserDisplayName";
+export { isTodayUnavailable } from "./isTodayUnavailable";
