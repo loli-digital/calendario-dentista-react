4e3c47b866cf97a7d58904833b7e13ab96d87983
loli-digital <loladigitalizada@gmail.com>
2026-09-23 14:05:57 +0200

    ♻️ refactor: Updated button styles for each state; added logic for the 'Delete' button and for each of the button's states.

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -7,6 +7,7 @@ export const Button = ({
   onClick,
   children,
   className = "",
+  deleteButton = false,
   disabled = false,
   ariaLabel,
   icon,
@@ -14,12 +15,20 @@ export const Button = ({
   ...props
 }) => {
   const baseStyles =
-    "inline-flex items-center justify-center gap-2 p-3 rounded-sm transition-all duration-200 focus:outline-none focus:ring-2 focus:ring-cyan-400 cursor-pointer";
-  const enabledStyles =
-    "bg-cyan-700 text-white shadow-[0_0_5px_black] hover:bg-cyan-800 hover:shadow-[0_0_5px_#fff]";
+    "inline-flex items-center justify-center gap-2 p-3 rounded-sm transition-all duration-200 text-white focus:outline-none focus-visible:ring-2 cursor-pointer shadow-[0_0_5px_black] hover:shadow-[0_0_5px_#fff]";
+  const defaultStyles =
+    "bg-cyan-700  hover:bg-cyan-800  focus-visible:ring-cyan-400";
   const disabledStyles = "bg-slate-400 text-slate-700 cursor-not-allowed";
+  const deletedStyles =
+    "bg-red-800 hover:bg-red-900 focus-visible:ring-red-950";
 
-  const classNames = `${baseStyles} ${disabled ? disabledStyles : enabledStyles} ${className}`;
+  const getStateStyles = () => {
+    if (disabled) return disabledStyles;
+    if (deleteButton) return deletedStyles;
+    return defaultStyles;
+  };
+
+  const classNames = `${baseStyles} ${getStateStyles()} ${className}`;
 
   const content = (
     <>
@@ -49,13 +58,13 @@ export const Button = ({
 
   return (
     <button
+      {...props}
       type={type}
       className={classNames}
       onClick={onClick}
       disabled={disabled}
       aria-label={ariaLabel}
       aria-disabled={disabled}
-      {...props}
     >
       {content}
     </button>
